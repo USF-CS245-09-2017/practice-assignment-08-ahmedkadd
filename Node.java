824e20d5d698cb2a40198879a48e3cb2a85b26dc
ahmedkadd <ahmedkaddoura99@gmail.com>
2019-03-29 09:59:18 -0700

    Created constructor

--- Node.java
+++ Node.java
@@ -1,4 +1,14 @@
 
-public class Node {
+public class Node<T> {
+	
+	private T data;
+	private Node<T> left;
+	private Node<T> right;
+	
+	public Node(T data) {
+		this.data = data;
+		left = null;
+		right = null;
+	}
 
 }
