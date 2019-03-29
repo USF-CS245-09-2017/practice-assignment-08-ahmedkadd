5b05806a91e202d4b68f445b334fa9b609f3803c
ahmedkadd <ahmedkaddoura99@gmail.com>
2019-03-29 10:14:48 -0700

    Implemented insert

--- BST.java
+++ BST.java
@@ -15,7 +15,22 @@ public class BST<T> {
 	}
 	
 	public void insert(Comparable<T> value) {
+		root = insert(root, value);
+	}
+	
+	public Node<T> insert(Node<T> root, Comparable<T> value) {
+		if (root == null) {
+			root = new Node(value);
+			return root;
+		}
 		
+		if (value.compareTo(root.data) < 0) {
+			root.left = insert(root.left, value);
+		} else if (value.compareTo(root.data) > 0) {
+			root.right = insert(root.right, value);
+		}
+		
+		return root;
 	}
 	
 	public void delete(Comparable<T> value) {
