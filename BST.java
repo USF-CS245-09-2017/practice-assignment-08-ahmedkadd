790cd031feb2526a395b7177b7c6b197a0e819eb
ahmedkadd <ahmedkaddoura99@gmail.com>
2019-04-04 12:31:26 -0700

    Comments

--- BST.java
+++ BST.java
@@ -1,16 +1,34 @@
 
+/**
+ * Binary search tree class
+ * 
+ * @param <T> type of data
+ */
 public class BST<T extends Comparable<T>> {
 	
+	/* Pointer to the root of the tree */
 	private Node<T> root;
 	
+	/*
+	 * Constructor creates an empty tree
+	 */
 	public BST() {
 		root = null;
 	}
 	
+	/*
+	 * Returns true if the value is in the tree, false if it is not. Starts the 
+	 * search by calling a recursive find function on the root of the tree.
+	 */
 	public boolean find(T value) {
 		return find(root, value);
 	}
 	
+	/*
+	 * Recursive function starts at the root node and searches down the tree. 
+	 * Returns false when a null node is found (value not in tree), and true 
+	 * if the value is in the tree.
+	 */
 	private boolean find(Node<T> root, T value) {
 		if (root == null) {
 			return false;
@@ -27,10 +45,18 @@ public class BST<T extends Comparable<T>> {
 		}		
 	}
 	
+	/*
+	 * Inserts the value into the tree by passing the root to the 
+	 * recursive insert function.
+	 */
 	public void insert(T value) {
 		root = insert(this.root, value);
 	}
 	
+	/*
+	 * Recursive function starts at the root node and searches down the tree for 
+	 * the position in which the value should be inserted and creates a new node. 
+	 */
 	private Node<T> insert(Node<T> root, T value) {
 		if (root == null) {
 			return new Node<T>(value);
@@ -45,10 +71,19 @@ public class BST<T extends Comparable<T>> {
 		return root;
 	}
 	
+	/*
+	 * Deletes the node with the given value in the tree if it exists.
+	 * Passes the root to the recursive delete function.
+	 */
 	public void delete(T value) {
 		root = delete(this.root, value);
 	}
 	
+	/*
+	 * Recursive function starts at the root node and searches down the tree for
+	 * a node with the given value. If found, deletes the node depending on how 
+	 * children it has.
+	 */
 	private Node<T> delete(Node<T> root, T value) {
 		if (root == null) {
 			return null;
@@ -60,10 +95,9 @@ public class BST<T extends Comparable<T>> {
 				return root.right;
 			} else if (root.right == null) {
 				return root.left;
-			}
+			} 
 			
 			root.setData(findSmallestChild(root.right)); 
-			
 			delete(root.right, root.getData());
 		}
 		
@@ -76,6 +110,9 @@ public class BST<T extends Comparable<T>> {
 		return root;
 	}
 	
+	/*
+	 * Returns the data of the smallest child of a node.
+	 */
 	private T findSmallestChild(Node<T> root) {
 		T min = root.getData();
 		
@@ -89,28 +126,22 @@ public class BST<T extends Comparable<T>> {
 		return min;
 	}
 	
+	/*
+	 * Calls the recursive print function to print the tree in ascending order.
+	 */
 	public void print() {
 		print(this.root);
 	}
 	
+	/*
+	 * Recursive function calls print on left child, prints the data in the node,
+	 * then calls print on the right child.  
+	 */
 	private void print(Node<T> root) {
 		if (root != null) {
 			print(root.left);
 			System.out.println(root.getData());
 			print(root.right);
 		}
-	}
-	
-	public static void main(String[] args) {
-		BST<Integer> tree = new BST<Integer>();
-		
-		tree.insert(7);
-		tree.insert(2);
-		tree.insert(8);
-		tree.insert(-3);
-		
-		tree.delete(8);
-		
-		tree.print();
 	}
 }
--- Node.java
+++ Node.java
@@ -1,20 +1,38 @@
 
+/**
+ * A binary search tree node
+ *
+ * @param <T> type of data
+ */
 public class Node<T extends Comparable<T>> {
 	
 	private T data;
+	
+	/* Pointer to the left node */
 	public Node<T> left;
+	
+	/* Pointer to the right node*/
 	public Node<T> right;
 	
+	/*
+	 * Constructor sets the data
+	 */
 	public Node(T data) {
 		this.data = data;
 		this.left = null;
 		this.right = null;
 	}
 	
+	/*
+	 * Returns the data
+	 */
 	public T getData() {
 		return this.data;
 	}
 	
+	/*
+	 * Sets the data to the given value
+	 */
 	public void setData(T value) {
 		this.data = value;
 	}
